e6b60d292fe39d2cc18d367bd6259a10cb49b420
Flora Petersen <floramarina@comcast.net>
2020-06-16 19:46:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,14 +9,4 @@ class Song
   def save
     self.class.all << self
   end
-
-  def Song.create
-    Song.all
-    name
-  end
-
-  def Song.all(name)
-    @name = name
-    @@all << @name
-  end
 end
