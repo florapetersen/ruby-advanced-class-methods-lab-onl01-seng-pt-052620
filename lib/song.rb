e79d5b6926a36e4e6007883c56006b01763b9155
Flora Petersen <floramarina@comcast.net>
2020-06-16 23:23:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,17 +15,18 @@ class Song
   def self.create
     #you don't need to set the song instance to a variable here
     #unless you want to do something with that variable right away
-    Song.new.save
+    self.new.save
   end
 
   def self.new_by_name(name)
-    song = Song.new
+    #use self instead of Song, in case you were using this with a different class
+    song = self.new
     song.name = name
     song
   end
 
   def self.create_by_name(name)
-    song = Song.new_by_name(name).save
+    song = self.new_by_name(name).save
   end
 
   def self.find_by_name(name)
